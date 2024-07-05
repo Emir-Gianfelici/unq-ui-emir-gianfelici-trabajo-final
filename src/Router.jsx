fdb32aefd495d81212053e2e91012504fef14db1
Emir-Gianfelici <emirgianfelici0@gmail.com>
2024-07-05 13:16:17 -0300

    add: Cambios en Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,33 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Questions from "./pages/Questions";
 
-import { useState, useEffect } from "react";
-import axios from "axios";
-
 const Router = () => {
-
-    const [difficulties, setDifficulties] = useState([]);
-
-    useEffect(() => {
-        const get_difficulties = async () => {
-            const url = "https://preguntados-api.vercel.app/api/difficulty";
-            const response = await axios.get(url);
-            setDifficulties(response.data);
-        };
-
-        get_difficulties();
-
-    }, [axios]);
 
     return (
 
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                {difficulties.map(
-                    difficulty => 
-                        <Route path={difficulty} element={<Questions difficulty={difficulty}/>} key={difficulty} />
-                    )}
+                <Route path={"/easy"} element={<Questions difficulty={"easy"}/>} />
+                <Route path={"/normal"} element={<Questions difficulty={"normal"}/>} />
+                <Route path={"/hard"} element={<Questions difficulty={"hard"}/>} />
+                <Route path={"/extreme"} element={<Questions difficulty={"extreme"}/>} />
             </Routes>
         </BrowserRouter>
 
