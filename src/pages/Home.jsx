cd2860f0c049221fff36f739c405f5cf87491ded
Emir-Gianfelici <emirgianfelici0@gmail.com>
2024-07-08 14:19:17 -0300

    add: Se agregan estilos para el componente Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,15 +8,19 @@ const Home = () => {
     const [difficulties, setDifficulties] = useState([]);
     const navigate = useNavigate();
     const [errorMessage, setErrorMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const get_difficulties = async () => {
+            setIsLoading(true);
             Api.get_difficulties()
                 .then(response => {
                     setDifficulties(response.data);
+                    setIsLoading(false);
                 })
                 .catch(error => {
                     setErrorMessage("Ocurrio un error al cargar la pagina.");
+                    setIsLoading(false);
                 });
         };
 
@@ -28,24 +32,43 @@ const Home = () => {
         navigate(`/${difficulty}`);
     }
 
+    const first_letter_to_uppercase = (string) => {
+
+        const first_letter = string.charAt(0);
+        const rest_string = string.substring(1);
+        return (first_letter.toUpperCase() + rest_string);
+    };
+
     return (
 
-        <div className="homeMainContainer">
-            <h1>Bienvenido!</h1>
-            <div className="errorMessage">
-                {errorMessage == "" ? "" : errorMessage}
-            </div>
-            <div>
-                <h2>Para comenzar, seleccione la dificultad de las preguntas: </h2>
-            </div>
-            <div>
-                {difficulties.map(
-                    difficulty =>
-                        <div className="difficultyButtonContainer" key={difficulty}>
-                            <button className="difficultyButton" onClick={() => go_to_questions_with_difficulty(difficulty)}>{difficulty}</button>
+        <div className="app">
+            <div className="homeMainContainer">
+                <h1 className="appTitle">Preguntados App</h1>
+                <div className="errorMessage">
+                    {errorMessage == "" ? "" : errorMessage}
+                </div>
+                <div>
+                    <h2 className="selectDifficultyText">Seleccione la dificultad de las preguntas: </h2>
+                </div>
+                <div>
+                    {
+                    isLoading ?
+                        <div className="loader"></div>
+                    :
+                        <div>
+                            {difficulties.map(
+                                difficulty =>
+                                    <div className="difficultyButtonContainer" key={difficulty}>
+                                        <button className="difficultyButton" onClick={() => go_to_questions_with_difficulty(difficulty)}>
+                                            <p>{first_letter_to_uppercase(difficulty)}</p>
+                                        </button>
+                                    </div>
+                            )}
                         </div>
-                )}
+                    }
+                </div>
             </div>
+            
         </div>
     );
 
